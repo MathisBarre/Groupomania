07c95ecefab1f8d77ed27768f4948cc7730757c2
Mathis Barré <m.barre@beapp.fr>
2021-12-25 19:46:29 +0100

    feat: add link to error page & improve demo instructions cards

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,12 +16,18 @@ export default function Home() {
       <div className="z-10 flex flex-col items-center justify-center w-full max-w-xl mx-auto">
         <Image src={Logo} layout="intrinsic" alt="groupomania" width="350" height="56" />
         <Tabs />
-        { process.env.NEXT_PUBLIC_DEMO_MODE && (<div className="text-white mt-8">
-          <h3 className="uppercase font-bold">Mode démonstration activé</h3>
-          <p>Compte de démo :</p>
+        { process.env.NEXT_PUBLIC_DEMO_MODE && (<div className="mt-4 w-full px-4 py-6 bg-white rounded-lg shadow sm:p-6">
+          <h2 className="uppercase font-bold">Mode démonstration activé</h2>
+          <h3 className="mt-2">Compte de démo :</h3>
           <ul className="list-disc ml-5">
             <li><strong>email:</strong> demo@mail.com</li>
             <li><strong>mdp:</strong> aA123456789</li>
+          </ul>
+
+          <h3 className="mt-2">Pages d&apos;erreurs :</h3>
+          <ul className="list-disc ml-5">
+            <li className="underline"><Link href="/404"><a>Page &quot;Erreur 404 : not found&quot;</a></Link></li>
+            <li className="underline"><Link href="/500"><a>Page &quot;Erreur 500 : internal server error&quot;</a></Link></li>
           </ul>
         </div>)}
       </div>
